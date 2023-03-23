172cc53f449258b9d1c293c7d29c206d27e64721
Mur <murmavilink@gmail.com>
2023-03-23 13:37:21 +0600

    added filter button click processing

--- src/modules/filter.js
+++ src/modules/filter.js
@@ -6,25 +6,28 @@ import { viewGoods } from "./viewGoods";
 export const filter = async () => {
     try {
         const goods = await getData();
-
+        const filter = document.querySelector('.filter');
+        const filterBtn = document.querySelector('.shop__filter-btn');
         let sordetData = goods;
-    
+
+
+
         const search = () => {
             const formSearch = document.querySelector('.filter-search__form');
             const formSearchBtn = document.querySelector('.filter-search__btn');
-    
+
             formSearchBtn.disabled = true;
-    
+
             formSearch.addEventListener('input', (e) => {
                 sordetData = goods.filter(item => {
                     const productName = item.name.toLowerCase().includes(e.target.value.toLowerCase());
                     const productCat = item.category.toLowerCase().includes(e.target.value.toLowerCase());
-    
+
                     if (productName) return productName;
                     if (productCat) return productCat;
                 });
-    
-    
+
+
                 if (!e.target.value) {
                     sordetData = goods;
                     render({ selectorWrap: '.shop-content__inner', stack: 10, sordetData });
@@ -32,41 +35,41 @@ export const filter = async () => {
                     viewGoods(true);
                     render({ selectorWrap: '.shop-content__inner', sordetData });
                 }
-                
+
             });
         };
-    
-    
+
+
         const price = () => {
             const formPrice = document.querySelector('.filter-price__form');
             const formPriceMin = formPrice.querySelector('#min');
             const formPriceMax = formPrice.querySelector('#max');
             const minElement = formPrice.querySelector('.filter-price__from');
             const maxElement = formPrice.querySelector('.filter-price__to');
-    
+
             let minValue;
             let maxValue;
-    
-    
+
+
             const addZero = () => {
                 if (!minElement.textContent) minElement.textContent = 0;
                 if (!maxElement.textContent) maxElement.textContent = 0;
             };
-    
-    
+
+
             const formattingValue = (value) => {
                 return value.toLocaleString("ru");
             };
-    
-    
+
+
             const removeSpaces = (value) => {
                 return value.replace(/\s+/g, '');
             };
-    
-    
+
+
             formPrice.addEventListener('input', (e) => {
                 e.preventDefault();
-    
+
                 if (e.target.id === 'min') {
                     minValue = removeSpaces(e.target.value);
                     minElement.textContent = formattingValue(+minValue);
@@ -74,43 +77,48 @@ export const filter = async () => {
                     maxValue = removeSpaces(e.target.value);
                     maxElement.textContent = formattingValue(+maxValue);
                 }
-    
+
                 if (!formPriceMin.value && !formPriceMax.value) {
                     sordetData = goods;
                     render({ selectorWrap: '.shop-content__inner', stack: 10, sordetData });
                 }
-    
+
                 viewGoods(true);
                 addZero();
             });
-    
-    
+
+
             formPrice.addEventListener('submit', (e) => {
                 e.preventDefault();
-    
+
                 sordetData = sordetData.filter(item => {
                     if (+removeSpaces(item.price) >= minValue && +removeSpaces(item.price) <= maxValue) return item;
                 });
-    
+
                 if (!sordetData[0]) sordetData = goods;
-    
+
                 viewGoods(true);
                 render({ selectorWrap: '.shop-content__inner', stack: 10, sordetData });
             });
-    
+
         };
-    
-    
+
+
+        filterBtn.addEventListener('click', () => {
+            !filter.style.display ?  filter.style.display = 'block' : filter.style.display = '';
+        });
+
+
         const filterCheckbox = (selectorForm, selectorInput, selectorText, selectorLabel, property) => {
             const filterWrap = document.getElementById(selectorForm);
             let sordetDataCheckbox = [];
-    
-    
+
+
             filterWrap.addEventListener('change', (e) => {
                 const checkedInput = e.target.closest(selectorLabel).querySelector(selectorInput);
                 const checkedText = checkedInput.parentNode.querySelector(selectorText).textContent.trim();
-    
-    
+
+
                 if (checkedInput.checked) {
                     goods.forEach(item => {
                         if (item[property] === checkedText) {
@@ -124,22 +132,25 @@ export const filter = async () => {
                         }
                     });
                 }
-    
-    
+
+
                 viewGoods(true);
                 render({ selectorWrap: '.shop-content__inner', sordetData: sordetDataCheckbox });
-                if(document.querySelector('.shop-content__inner').childElementCount === 0) render({ selectorWrap: '.shop-content__inner', stack: 10, sordetData });
-                
+                if (document.querySelector('.shop-content__inner').childElementCount === 0) render({ selectorWrap: '.shop-content__inner', stack: 10, sordetData });
+
             });
         };
-    
-    
+
+
         search();
         price();
         filterCheckbox('filter-size', '.filter-size__input', '.filter-size__text', '.filter-size__label', 'size');
         filterCheckbox('filter-category', '.filter-category__input', '.filter-category__checkbox', '.filter-category__label', 'category');
         filterCheckbox('filter-gender', '.filter-popular__input', '.filter-popular__checkbox', '.filter-popular__label', 'gender');
-    } catch (error) {}
+    } catch (error) {
+        console.log(error.message);
+    }
+    
 };
 
 
